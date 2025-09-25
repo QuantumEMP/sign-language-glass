0eeba99c4aea2a088ffcf3eaf22c7f4867d2308e
Jude Rose <jurosecpt024@student.wethinkcode.co.za>
2025-09-25 14:35:54 +0200

    text to video added

--- src/main/java/basic.java
+++ src/main/java/basic.java
@@ -1,15 +1,37 @@
 import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.*;
+
+import java.util.List;
 
 public class basic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Client client = Client.builder()
                 .apiKey("AIzaSyCV3nyovUpBoa9LY4be-MS42AglxVgyMMM")
                 .build();
 
         GenerateContentResponse response =
-                client.models.generateContent("gemini-2.5-flash", "what is \"hello\" in south african sign language", null);
+                client.models.generateContent("gemini-2.5-flash", "create a prompt to create a video of a person that signs \"hello\" in south african sign language", null);
 
         System.out.println(response.text());
+
+        GenerateVideosOperation operation = client.models.generateVideos("veo-3.0-fast-generate-001", GenerateVideosSource.builder()
+                        .prompt(response.text())
+                        .build(),
+                GenerateVideosConfig.builder()
+                        .aspectRatio("16:9")
+                        .resolution("720p")
+                        .build());
+
+        while (!operation.done().orElse(false)) {
+            Thread.sleep(1000);
+            operation = client.operations.getVideosOperation(operation, null);
+        }
+
+        operation.response()
+                .flatMap(GenerateVideosResponse::generatedVideos)
+                .stream()
+                .flatMap(List::stream)
+                .findFirst()
+                .ifPresent(video -> client.files.download( video, "video.mp4", null));
     }
 }
